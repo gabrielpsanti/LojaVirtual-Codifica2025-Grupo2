93f7005f9e5f45bc93dade4f8de7a76c299b7fd5
Chloe <raionbyjinn@gmail.com>
2026-04-09 20:57:40 -0300

    Criação dos arquivos das views (telas) e das views e classes dos componentes

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<div id="header">
+    <div id="bloco-superior">
+        <span>
+            search
+        </span>
+        <span></span>
+        <span></span>
+    </div>
+    <div id="bloco-inferior">
+        <div id="navbar">
+
+        </div>
+    </div>
+</div>
