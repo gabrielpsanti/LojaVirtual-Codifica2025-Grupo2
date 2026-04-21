dead37daf9f4fbd26bdbbc36c6d43f30f3bf3a2b
Chloe <raionbyjinn@gmail.com>
2026-04-21 03:57:58 -0300

    Mudanças na navbar, preparativos pra fazer o dropdown menu

--- resources/views/components/cliente/navbar.blade.php
+++ resources/views/components/cliente/navbar.blade.php
@@ -1,32 +1,66 @@
-<ul id="navbar-list" class="w-auto no-underline flex justify-around gap-8">
+<ul id="navbar-list" class="w-auto no-underline flex items-center justify-around gap-8">
     <li class="navbar-item">
-        <a href="{{ route('teste') }}">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta hover:text-bold">
             Início
         </a>
     </li>
     <li class="navbar-item ">
-        <a href="{{ route('teste') }}">
-            Produtos
-        </a>
+        <div>
+            <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg flex gap-2">
+                Produtos
+                <span>
+                    <svg class="size-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M32 288c-12.9 0-24.6 7.8-29.6 19.8S.2 333.5 9.4 342.6l160 160c12.5 12.5 32.8 12.5 45.3 0l160-160c9.2-9.2 11.9-22.9 6.9-34.9S364.9 288 352 288L32 288z"/></svg>
+                </span>
+            </a>
+        </div>
+{{--        <div class="w-full bg-lilas-300">--}}
+{{--            <ul>--}}
+{{--                <li>--}}
+{{--                    <a href="">--}}
+{{--                        oi--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="">--}}
+{{--                        oi--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="">--}}
+{{--                        opi--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="">--}}
+{{--                        oi--}}
+{{--                    </a>s--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="">--}}
+{{--                        oi--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
 
     </li>
     <li class="navbar-item ">
-        <a href="{{ route('teste') }}">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta">
             Contato
         </a>
     </li>
     <li class="navbar-item ">
-        <a href="{{ route('teste') }}">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta">
             FAQ
         </a>
     </li>
     <li class="navbar-item ">
-        <a href="{{ route('teste') }}">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta">
             Trocas e Devoluções
         </a>
     </li>
     <li class="navbar-item ">
-        <a href="{{ route('teste') }}">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta">
             Sobre nós
         </a>
     </li>
