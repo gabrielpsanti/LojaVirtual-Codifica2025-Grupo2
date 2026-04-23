7bf1abcfbfc7d876945e1b355d58742bad821468
Chloe <raionbyjinn@gmail.com>
2026-04-23 20:14:54 -0300

    commit

--- resources/views/components/cliente/navbar.blade.php
+++ resources/views/components/cliente/navbar.blade.php
@@ -1,47 +1,49 @@
 <ul id="navbar-list" class="w-auto no-underline flex items-center justify-around gap-8">
     <li class="navbar-item">
-        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta hover:text-bold">
+        <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg hover:bg-rosa-100 hover:text-azul-paleta">
             Início
         </a>
     </li>
     <li class="navbar-item ">
+
         <div>
-            <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg flex gap-2">
+            <a href="{{ route('teste') }}" class="px-3 py-2 rounded-lg flex gap-2 hover:bg-rosa-100 hover:text-azul-paleta" onmouseover="mostraCategorias()">
                 Produtos
                 <span>
                     <svg class="size-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M32 288c-12.9 0-24.6 7.8-29.6 19.8S.2 333.5 9.4 342.6l160 160c12.5 12.5 32.8 12.5 45.3 0l160-160c9.2-9.2 11.9-22.9 6.9-34.9S364.9 288 352 288L32 288z"/></svg>
                 </span>
             </a>
         </div>
-{{--        <div class="w-full bg-lilas-300">--}}
-{{--            <ul>--}}
-{{--                <li>--}}
-{{--                    <a href="">--}}
-{{--                        oi--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="">--}}
-{{--                        oi--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="">--}}
-{{--                        opi--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="">--}}
-{{--                        oi--}}
-{{--                    </a>s--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="">--}}
-{{--                        oi--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
+
+        <div class="absolute size-auto bg-lilas-300">
+            <ul class="w-auto no-underline flex items-center justify-around gap-10">
+                <li>
+                    <a href="">
+                        oi 1
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        oi 2
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        oi 3
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        oi 4
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        oi 5
+                    </a>
+                </li>
+            </ul>
+        </div>
 
     </li>
     <li class="navbar-item ">
@@ -65,3 +67,11 @@
         </a>
     </li>
 </ul>
+
+<script>
+
+    function mostraCategorias() {
+
+    }
+
+</script>
