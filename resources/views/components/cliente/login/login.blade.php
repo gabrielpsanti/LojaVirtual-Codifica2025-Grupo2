aef5e1c04bd0ec8e92be5eaea868eeb60c0abc8c
Lucas <lucas.rocha9@outlook.com>
2026-04-26 22:54:16 -0300

    Fiz o cartão 34

--- resources/views/components/cliente/login/login.blade.php
+++ resources/views/components/cliente/login/login.blade.php
@@ -2,19 +2,19 @@
 <title>Login</title>
 <div class="fundo">
     <div class="fundo-caixa">
-        <form class="fundo-caixa-formulario" method="POST" > 
-                    @csrf
-                    <label for="email">E-mail</label>
-                    <input type="email" name="email" id="email" value="{{ old('email') }}">
-                    
-                    <label for="password">Senha</label>
-                    <input type="password" name="password" id="password">
-                    
-                    @error('email')
-                        <span class="fundo-caixa-formulario-error">{{ $message }}</span>
-                    @enderror
-                    <button>Entrar</button>
-                    <a href="{{ route('cadastro') }}">Registrar</a>
+        <form class="fundo-caixa-formulario" method="POST" action="{{ route('signin') }}">
+            @csrf
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password">
+
+            @error('email')
+                <span class="fundo-caixa-formulario-error">{{ $message }}</span>
+            @enderror
+            <button>Entrar</button>
+            <a href="{{ route('cadastro') }}">Registrar</a>
         </form>
-    <div>
-</div>
+        <div>
+        </div>
